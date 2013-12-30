a3068d14be946f8bd28237486c531392801be39d
sebastinantony <arun.antony@live.com>
2013-12-30 18:47:54 +0530

    Design Updated

---
+++ GoogleClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialProfiles
+{
+    public class GoogleClient
+    {
+        public static string clientID = "108169322756-pbgv2b3ng77akofik74airq8kffao5ou.apps.googleusercontent.com";
+        public static string scope = "https://www.google.com/m8/feeds/&approval_prompt=force&access_type=offline";
+    }
+}
